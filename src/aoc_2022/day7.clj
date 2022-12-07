060cc94cff588b452cb521b9d9224fcad7425032
Antti Slunga <antti_slunga@hotmail.com>
2022-12-07 18:23:30 +0200

    Day 7 Part 2

--- src/aoc_2022/day7.clj
+++ src/aoc_2022/day7.clj
@@ -43,40 +43,53 @@
 (defn file? [value]
   (not (or (dir? value) (command? value))))
 
+(defn make-path [location]
+  (keyword (reduce (fn [v a] (if (= "/" v)
+                               (str v a)
+                               (str v "/" a))) location)))
+
+(defn folders [input]
+  (loop [input input
+         location []
+         output {}]
+    (if (empty? input)
+      output
+      (recur (rest input)
+             (let [command (first input)]
+               (if (cd-command? command)
+                 (let [dir (last (str/split command #" "))]
+                   (if (= ".." dir)
+                     (pop location)
+                     (conj location dir)))
+                 location))
+             (let [command (first input)]
+               (cond
+                 (file? command)
+                 (loop [output output
+                        location location]
+                   (if (empty? location)
+                     output
+                     (recur
+                       (assoc output (make-path location)
+                                     (+ (get output (make-path location) 0) (Integer/parseInt (first (str/split command #" ")))))
+                       (pop location))))
+
+                 :else
+                 output))))))
+
 (defn part1 []
   (let [input (get-input)]
-
-    (->> (loop [input input
-                location []
-                output {}]
-           (if (empty? input)
-             output
-             (recur (rest input)
-                    (let [command (first input)]
-                      (if (cd-command? command)
-                        (let [dir (last (str/split command #" "))]
-                          (if (= ".." dir)
-                            (pop location)
-                            (conj location (keyword dir))))
-                        location))
-                    (let [command (first input)
-                          _ (println location)]
-                      (cond
-                        (file? command)
-                        (loop [output output
-                               location location]
-                          (if (empty? location)
-                            output
-                            (recur
-                              (assoc output (keyword (str/replace (apply str location) ":" "/"))
-                                            (+ (get output (keyword (str/replace (apply str location) ":" "/")) 0) (Integer/parseInt (first (str/split command #" ")))))
-                              (pop location))))
-
-                        :else
-                        output)))))
+    (->> (folders input)
          vals
          (mapv #(if (<= % 100000) % 0))                     ;;1419174 1506020 1778099
-         (apply +)
-         )
-    ;(tree-seq dir? #(do (println %) (seq %)) input)
-    ))
+         (apply +))))
+
+(defn part2 []
+  (let [disk-size 70000000
+        used-space (:/ (folders (get-input)))
+        required-space (- 30000000 (- disk-size used-space))]
+    (->> (folders (get-input))
+         (filter #(>= (val %) required-space))
+         (mapv val)
+         sort
+         first)))                                           ;; => 1623571
