b172ce027c35a08c27f4b6f4d2a070443674cd21
Antti Slunga <antti_slunga@hotmail.com>
2022-12-08 22:51:31 +0200

    Day 8 Part 1.5

--- src/aoc_2022/day8.clj
+++ src/aoc_2022/day8.clj
@@ -1,8 +1,13 @@
 (ns aoc-2022.day8
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
 
-(defn input []
-  (slurp (io/resource "trees.txt")))
+(defn input-from-file []
+  (mapv
+    (fn [v]
+      (mapv #(Integer/parseInt %) (str/split v #"")))
+    (-> (slurp (io/resource "trees.txt"))
+        (str/split #"\n"))))
 
 (def test-input
   [[3 0 3 7 3]
@@ -11,11 +16,15 @@
    [3 3 5 4 9]
    [3 5 3 9 0]])
 
+(def input
+  #_(input-from-file)
+  test-input)
+
 (def last-row
-  (dec (count test-input)))
+  (dec (count input)))
 
 (def last-column
-  (dec (count (first test-input))))
+  (dec (count (first input))))
 
 (defn on-the-edge? [row column]
   (or (or (= row 0) (= row last-row))
@@ -23,24 +32,79 @@
 
 (defn visible? [row column tree]
   (let [visible-from-top? (every? true? (for [rowc (range row)]
-                                          (< (nth (nth test-input rowc) column) tree)))
+                                          (< (nth (nth input rowc) column) tree)))
         visible-from-down? (every? true? (for [rowc (range (inc row) (inc last-row))]
-                                           (< (nth (nth test-input rowc) column) tree)))
+                                           (< (nth (nth input rowc) column) tree)))
         visible-from-left? (every? true? (for [columnn (range column)]
-                                           (< (nth (nth test-input row) columnn) tree)))
+                                           (< (nth (nth input row) columnn) tree)))
         visible-from-right? (every? true? (for [columnn (range (inc column) (inc last-column))]
-                                            (< (nth (nth test-input row) columnn) tree)))]
+                                            (< (nth (nth input row) columnn) tree)))]
     (or visible-from-top?
         visible-from-down?
         visible-from-left?
         visible-from-right?)))
 
+(defn count-trees [trees]
+  (if (empty? trees)
+    0
+    (let [_ (println "[" trees "]")
+          ind (.indexOf trees 2)]
+      (if (= (first trees) 0)
+        1
+        (if (= ind -1)
+          (apply + trees)
+          (->> (take (inc ind) trees)
+               (apply +)
+               dec))))))
+(defn scenic-score [row column tree]
+  (let [up (for [rowc (reverse (range row))]
+             (cond
+               (< (nth (nth input rowc) column) tree) 1
+               (= (nth (nth input rowc) column) tree) 2
+               (> (nth (nth input rowc) column) tree) 0))
+        up (count-trees up)
+        down (for [rowc (range (inc row) (inc last-row))]
+               (cond
+                 (< (nth (nth input rowc) column) tree) 1
+                 (= (nth (nth input rowc) column) tree) 2
+                 (> (nth (nth input rowc) column) tree) 0))
+        down (count-trees down)
+        left (for [columnn (reverse (range column))]
+               (cond
+                 (< (nth (nth input row) columnn) tree) 1
+                 (= (nth (nth input row) columnn) tree) 2
+                 (> (nth (nth input row) columnn) tree) 0))
+        left (count-trees left)
+        right (for [columnn (range (inc column) (inc last-column))]
+                (cond
+                  (< (nth (nth input row) columnn) tree) 1
+                  (= (nth (nth input row) columnn) tree) 2
+                  (> (nth (nth input row) columnn) tree) 0))
+        right (count-trees right)
+        _ (println up down left right)]
+    (* up down left right)))
+
 (defn part1 []
-  (->> test-input
-       (map-indexed
-         (fn [row v]
-           (map-indexed
-             (fn [column tree]
-               (if-not (on-the-edge? row column)
-                 (if (visible? row column tree) 1 0)
-                 "^")) v)))))
+  (let [trees-on-the-edge (+ (* 2 (count (first input))) (* 2 (- (count input) 2)))
+        trees-on-grid (->> input
+                           (map-indexed
+                             (fn [row v]
+                               (map-indexed
+                                 (fn [column tree]
+                                   (if-not (on-the-edge? row column)
+                                     (if (visible? row column tree) 1 0)
+                                     0)) v)))
+                           flatten
+                           (apply +))]
+    (+ trees-on-the-edge trees-on-grid)))                   ;=> 1647
+
+(defn part2 []
+  (let [scores (->> input
+                    (map-indexed
+                      (fn [row v]
+                        (map-indexed
+                          (fn [column tree]
+                            (scenic-score row column tree)) v)))
+                    (mapv #(into [] %))
+                    (into []))]
+    scores))
